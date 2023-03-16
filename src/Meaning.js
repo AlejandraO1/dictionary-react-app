63bdbed6578d298b83b0f5b3792d8af0e38c5477
AlejandraO1 <alejandraortegax@gmail.com>
2023-03-16 12:38:27 -0400

    Changed styling of meanings section

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,12 +5,12 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        Definition: {props.meaning.definition} <br />
-        {props.meaning.example}
+      <span>
+        <strong>Definition: {props.meaning.definition}</strong> <br />
+        <em>{props.meaning.example}</em>
         <br />
-        <Synonyms synonyms={props.meaning.synonyms} />
-      </p>
+      </span>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
